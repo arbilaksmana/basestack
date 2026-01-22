da520150a2c7aa62b005d08dc67dde5c4cd7b6ed
Muhammad Kafaby <kafaby@student.telkomuniversity.ac.id>
2026-01-22 18:27:35 +0700

    udah bisa running

--- frontend/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"yZKa6y/OFrupVC9TdfY3fXCfKdtCZITe/ldU3zDqbbo=\"\n}"
--- frontend/.next/dev/static/chunks/app_signup_page_tsx_ab3f932e._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_7772d2f8._.js",
-  "static/chunks/app_8e234e00._.js"
-],
-    source: "dynamic"
-});
